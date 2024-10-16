e7660796f66571b326a4bf2c8d5c9370ae60eb05
Serge Harb <me@sergeharb.com>
2024-10-16 16:50:58 +0200

    prevent double capture

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,6 +7,7 @@ if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'always',
+    capture_pageview: false,
   });
 }
 
