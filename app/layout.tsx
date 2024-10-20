5772003b683e0c67fb2317365a0845457c3b9acc
Serge Harb <me@sergeharb.com>
2024-10-20 22:17:21 +0200

    add more images

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,10 @@ export const metadata: Metadata = {
   description:
     "Perla is a professional chef and owner of Perla's Kitchen. She sells authentic Lebanese desserts and meals, bringing the taste of home to your table.",
   openGraph: {
-    images: [`${baseUrl}/images/white-bg-logo-square-200.jpg`],
+    images: [`${baseUrl}/images/logo_300x300.jpg`],
   },
   twitter: {
-    images: [`${baseUrl}/images/white-bg-logo-square-200.jpg`],
+    images: [`${baseUrl}/images/logo_300x300.jpg`],
   },
 };
 
