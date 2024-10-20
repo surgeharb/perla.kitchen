d38aec8181699713bd038fbdd40eb4c7716ce80b
Serge Harb <me@sergeharb.com>
2024-10-20 21:56:46 +0200

    add squayre metadata img

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,10 @@ export const metadata: Metadata = {
   description:
     "Perla is a professional chef and owner of Perla's Kitchen. She sells authentic Lebanese desserts and meals, bringing the taste of home to your table.",
   openGraph: {
-    images: [`${baseUrl}/images/white-bg-logo.jpg`],
+    images: [`${baseUrl}/images/white-bg-logo-square.jpg`],
   },
   twitter: {
-    images: [`${baseUrl}/images/white-bg-logo.jpg`],
+    images: [`${baseUrl}/images/white-bg-logo-square.jpg`],
   },
 };
 
